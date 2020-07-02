578c987f836a5d72fead7a7bb8be215197faeff5
蓝铮 <yuman.ym@alibaba-inc.com>
2020-07-02 19:43:32 +0800

    [update] 优化代码

--- YMGCDTimer/YMGCDTimer/YMGCDTimer.m
+++ YMGCDTimer/YMGCDTimer/YMGCDTimer.m
@@ -42,13 +42,14 @@ typedef NS_ENUM(NSInteger, YMGCDTimerState) {
         _lock = [[NSLock alloc] init];
         NSUInteger times = ceil(interval * 10);
         _interval = interval / times;
-        _timer = nil;
+        dispatch_queue_t queue = nil;
         if (times == 1) {
-            _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+            queue = dispatch_get_main_queue();
         } else {
             const char *label = [[NSString stringWithFormat:@"com.YMGCDTimer.%p", self] UTF8String];
-            _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_queue_create(label, DISPATCH_QUEUE_SERIAL));
+            queue = dispatch_queue_create(label, DISPATCH_QUEUE_SERIAL);
         }
+        _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
         __weak __typeof__(self) weakSelf = self;
         dispatch_source_set_event_handler(_timer, ^{
             __strong __typeof__(self) self = weakSelf;
